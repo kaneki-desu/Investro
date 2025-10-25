e737374a96593f17415c664eb0abf3c083a31ee2
kaneki-desu <sibajit.mazumdar@gmail.com>
2025-10-25 10:14:26 +0530

    signing key added

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -2,5 +2,6 @@ import {Inngest} from 'inngest'
 
 export const inngest = new Inngest({
     id: 'investro',
+    signingKey: process.env.INNGEST_SIGNING_KEY, 
     ai: {gemini:{apiKey: process.env.GEMINI_API_KEY !}},
 })
